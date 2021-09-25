e30fe0b1de575a1fbfff3e4aee89398c75c14ade
Jannat267 <jannatulferdouserupali@gmail.com>
2021-09-26 01:23:07 +0600

    header

---
+++ src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './header.css';
+const Header = () => {
+    return (
+        <div className='container text-white'>
+             <div className='header text-center p-4 border rounded mt-4'>
+                   <h1>Galaxy It</h1>
+                   <p><strong>The list of an It Company employer .</strong></p>
+                   <h2>Total Budget : <b> 2 Million</b></h2>
+                </div>
+        </div>
+               
+            );
+};
+
+export default Header;
